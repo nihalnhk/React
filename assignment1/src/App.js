52470cfc7e6121baddd278bda1e1ec07f4dcf5bd
NIHAL K <61180857+nihalnhk@users.noreply.github.com>
2021-08-17 23:04:49 +0530

    Assignment 1

---
+++ assignment1/src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import C1 from "./c1";
+import C2 from "./C2";
+
+function App() {
+  return (
+    <div>
+      <h1>Main</h1>
+      <C1 />
+      <C2 />
+    </div>
+  );
+}
+
+export default App;
---
+++ assignment1/src/c1.js
@@ -0,0 +1,9 @@
+function C1() {
+  return (
+    <div>
+      <h1>From component 1</h1>
+    </div>
+  );
+}
+
+export default C1;
